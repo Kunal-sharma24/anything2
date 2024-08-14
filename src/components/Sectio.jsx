dee471f7fb2c66f9e67817674e8bbf6a292d97cd
farhanadil1 <imfarhan574@gmail.com>
2024-08-14 23:24:01 +0530

    footer update

--- src/components/Sectio.jsx
+++ src/components/Sectio.jsx
@@ -2,8 +2,62 @@ import React from 'react'
 
 export const Sectio = () => {
   return (
-        <div className='w-full h-[180px] bg-[#F1F6F8] absolute inset-x-0 bottom-0 rounded-sm'>
-            hello
+        <div className='w-full h-[280px] bg-[#DEE2E5] absolute inset-x-0 bottom-0 rounded-t-[40px]'>
+            <div className='flex justify-between mx-[150px] my-10 ' >
+              
+              <div className='font-bold text-[30px] text-[#3CA9C0]'>Anything
+                <b>
+                  <div className='font-normal text-[15px] text-black mb-2'>www.anything.com</div>
+                </b>
+              </div>
+              <div className='font-medium text-[20px] my-3'>About Us
+                
+                  <b>
+                    <div className='font-normal text-[15px] text-black my-1 hover:underline hover:text-[#3CA9C0]'><a href="http://www.anything.com">Contact Us</a></div>
+                  </b>
+                  <b>
+                    <div className='font-normal text-[15px] text-black hover:underline hover:text-[#3CA9C0]'><a href="http://www.gmail.com">Mail Us</a></div>
+                  </b>
+              </div>
+              <div className='font-medium text-[20px] my-3'>Products
+                
+              <b>
+                    <div className='font-normal text-[15px] text-black my-1 hover:underline hover:text-[#3CA9C0]'><a href="http://www.anything.com">Clothes</a></div>
+                  </b>
+                  <b>
+                    <div className='font-normal text-[15px] text-black hover:underline hover:text-[#3CA9C0]'><a href="http://www.gmail.com">Smartphones</a></div>
+                  </b>
+                  <b>
+                    <div className='font-normal text-[15px] text-black my-1 hover:underline hover:text-[#3CA9C0]'><a href="http://www.anything.com">Laptops</a></div>
+                  </b>
+                  <b>
+                    <div className='font-normal text-[15px] text-black hover:underline hover:text-[#3CA9C0]'><a href="http://www.gmail.com">Headphones</a></div>
+                  </b>
+                  <b>
+                    <div className='font-normal text-[15px] text-black my-1 hover:underline hover:text-[#3CA9C0]'><a href="http://www.anything.com">Smart Watches</a></div>
+                  </b>
+                  <b>
+                    <div className='font-normal text-[15px] text-black hover:underline hover:text-[#3CA9C0]'><a href="http://www.gmail.com">Shoes</a></div>
+                  </b>
+              </div>
+              <div className='font-medium text-[20px] my-3'>Help
+              <b>
+                    <div className='font-normal text-[15px] text-black my-1 hover:underline hover:text-[#3CA9C0]'><a href="http://www.anything.com">FAQ</a></div>
+                  </b>
+                  <b>
+                    <div className='font-normal text-[15px] text-black hover:underline hover:text-[#3CA9C0]'><a href="http://www.gmail.com">Shipping</a></div>
+                  </b>
+                  <b>
+                    <div className='font-normal text-[15px] text-black my-1 hover:underline hover:text-[#3CA9C0]'><a href="http://www.anything.com">Customer Care</a></div>
+                  </b>
+              </div>
+              
+            </div>
+           
+
+
         </div>   
   )
 }
+
+
