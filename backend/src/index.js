e9e5d625c18ab7ac8e39e53de4cfdddf63714f33
Md Adil Farhan <imfarhan574@gmail.com>
2025-02-27 20:52:36 +0530

    add nodemon as a dev dependency and enhance server startup error handling

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,3 +8,11 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("Server Error:", error);
+})
