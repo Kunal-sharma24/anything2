3a2748527c81485ce6d60c98bb658807e5d4d080
Md Adil Farhan <imfarhan574@gmail.com>
2025-02-27 20:46:11 +0530

    utils configured

---
+++ backend/src/utils/ApiResponse.js
@@ -0,0 +1,11 @@
+class ApiResponse{
+    constructor(
+        statusCode, 
+        message="success", 
+        data){
+        this.status = statusCode
+        this.message = message
+        this.data = data
+        this.success = statusCode < 400
+    }
+}
